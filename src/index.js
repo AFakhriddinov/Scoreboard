241254f6f20090db219e34bd1b5710828909ec60
AFakhriddinov <112176053+AFakhriddinov@users.noreply.github.com>
2023-04-19 06:31:10 +0500

    Set up project with Webpack and necessary files

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import './style.css';
+import playersData from './modules/data.js';
+
+const scores = document.querySelector('#scores');
+
+playersData.forEach((data, i) => {
+  if (i % 2 === 1) {
+    scores.innerHTML += `
+        <div class="player-details even">${playersData[i].name} : ${playersData[i].score}</div>
+    `;
+  } else {
+    scores.innerHTML += `
+        <div class="player-details">${playersData[i].name} : ${playersData[i].score}</div>
+    `;
+  }
+});
