62cfd1050715a9c87e619370a00bd5b53ad12b6b
AFakhriddinov <112176053+AFakhriddinov@users.noreply.github.com>
2023-04-19 12:05:43 +0500

    Simplify JS operators

--- src/index.js
+++ src/index.js
@@ -5,13 +5,8 @@ import playersData from './modules/data.js';
 const scores = document.querySelector('#scores');
 
 playersData.forEach((data, i) => {
-  if (i % 2 === 1) {
-    scores.innerHTML += `
-        <div class="player-details even">${playersData[i].name} : ${playersData[i].score}</div>
-    `;
-  } else {
-    scores.innerHTML += `
-        <div class="player-details">${playersData[i].name} : ${playersData[i].score}</div>
-    `;
-  }
+  scores.innerHTML
+    += i % 2 === 1
+      ? `<div class="player-details even">${playersData[i].name} : ${playersData[i].score}</div>`
+      : `<div class="player-details">${playersData[i].name} : ${playersData[i].score}</div>`;
 });
